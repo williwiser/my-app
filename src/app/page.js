bae5b8f32fc6811dada4bc3304c574cb691675ad
William <williwiser@gmail.com>
2024-05-15 19:50:57 +0200

    added body content

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,12 @@ export default function Home() {
         <h1>William Wani</h1>
         <a href="#">Contact</a>
       </div>
+      <section className={styles.sn}>
+        <div className={styles.description}>
+          <h1>Hi there, I made a website for CS3</h1>
+          <p>Just your average developer, nothing too fancy...</p>
+        </div>
+      </section>
     </main>
   );
 }
